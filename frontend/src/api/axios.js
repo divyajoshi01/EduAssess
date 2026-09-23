060ffaf1d28f2115948e3542e1bb18bc82e64193
bestofdivya <wayfordivya@gmail.com>
2026-09-23 20:47:16 +0530

    Fix API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_URL);
 const API = axios.create({
     // baseURL:"http://localhost:5000/api"
-     baseURL: import.meta.env.VITE_API_URL
+    baseURL: `${import.meta.env.VITE_API_URL}/api`
 });
 
 export default API;
